268dd337c77e25c9d65e1adf8f394aa1a479ac9f
Ankit Yadav <147944697+Ankityadav6386@users.noreply.github.com>
2024-11-14 10:50:18 +0530

    Time: 0 ms (100.00%), Space: 33.4 MB (88.24%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int maxVal(TreeNode* root){
+    TreeNode* temp = root;
+    if(temp == NULL){
+        return -1;
+    }
+    while(temp->right!=NULL){
+        temp = temp->right;
+    }
+    return temp-> val;
+}
+    TreeNode* deleteNode(TreeNode* root, int key) {
+    if(root == NULL){
+        return NULL;
+    } 
+    if(root->val == key){
+    if(root->left==NULL && root->right==NULL){
+        return NULL;
+    }
+    else if(root->left==NULL && root->right!=NULL){
+        TreeNode* child = root->right;
+        return child;
+    }
+    else if(root->left!=NULL && root->right==NULL){
+        TreeNode* child = root->left;
+        return child;
+    }
+    else{
+        int inorderpre = maxVal(root->left);
+        root->val = inorderpre;
+        root->left = deleteNode(root->left , inorderpre);
+        return root;
+    }
+    } 
+    else if(key > root->val){
+        root->right = deleteNode(root->right, key);
+    } 
+    else if(key < root->val){
+        root->left = deleteNode(root->left, key);  
+    }
+    return root; 
+    }
+};
